54d8546060c08ed41c62908e307e8df5b75637fe
vel <vellius5@gmail.com>
2023-12-19 02:39:01 -0800

    added hide/reveal methods to card class

--- blackjack/classes.py
+++ blackjack/classes.py
@@ -3,14 +3,23 @@ import random
 import constants as con
 
 class Card:
-    def __init__(self, rank, suit) -> None:
+    def __init__(self, rank, suit, hidden=False) -> None:
         self.rank = rank
         self.suit = suit
-        self.flipped = False
+        self.hidden = hidden
+        self.saved_values = None
 
     def hide_card(self):
+        self.saved_values = (self.rank, self.suit)
+
         self.rank = '##'
         self.suit = '###'
+        self.hidden = True
+
+    def reveal_card(self):
+        self.rank = self.saved_values[0]
+        self.suit = self.saved_values[1]
+        self.hidden = False
 
     def generate_text(self):
         card_text = ['-------  ', f'|{self.rank:<5}|  ', f'|{self.suit:^5}|  ', f'|{self.rank:>5}|  ', '-------  ']
